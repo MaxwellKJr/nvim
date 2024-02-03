3b57bc80a46e15e8750fd5181f1ce21d1048d287
MaxwellKJr <maxwellkjr@gmail.com>
2024-02-03 02:14:43 +0200

    Made changes to the gruvbox theme

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.o.background = "dark"
+vim.o.termguicolors = true
 vim.cmd([[
   hi NotifyBackground guibg = #3c3836
 ]])
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,7 +5,29 @@ return {
     priority = 1000,
     config = true,
     opts = {
-      transparent_mode = true,
+      transparent_mode = false,
+      terminal_colors = true, -- add neovim terminal colors
+      contrast = "soft", -- can be "hard", "soft" or empty string
+    },
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "transparent", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+      },
     },
   },
   {
